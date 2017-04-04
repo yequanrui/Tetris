fb2cf8a5d73bcabc57e4314d707348330701aa4a
yequanrui <yequanrui@qq.com>
2017-04-04 18:15:27 +0800

    rebuild files

---
+++ Tetris/about.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Tetris
+{
+    partial class about
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_author = new System.Windows.Forms.Label();
+            this.lbl_name = new System.Windows.Forms.Label();
+            this.lbl_time = new System.Windows.Forms.Label();
+            this.lbl_tool = new System.Windows.Forms.Label();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_author
+            // 
+            this.lbl_author.AutoSize = true;
+            this.lbl_author.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbl_author.Location = new System.Drawing.Point(22, 42);
+            this.lbl_author.Name = "lbl_author";
+            this.lbl_author.Size = new System.Drawing.Size(68, 17);
+            this.lbl_author.TabIndex = 1;
+            this.lbl_author.Text = "作者：小叶";
+            // 
+            // lbl_name
+            // 
+            this.lbl_name.AutoSize = true;
+            this.lbl_name.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbl_name.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.lbl_name.Location = new System.Drawing.Point(36, 13);
+            this.lbl_name.Name = "lbl_name";
+            this.lbl_name.Size = new System.Drawing.Size(127, 22);
+            this.lbl_name.TabIndex = 0;
+            this.lbl_name.Text = "C#版俄罗斯方块";
+            // 
+            // lbl_time
+            // 
+            this.lbl_time.AutoSize = true;
+            this.lbl_time.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbl_time.Location = new System.Drawing.Point(22, 66);
+            this.lbl_time.Name = "lbl_time";
+            this.lbl_time.Size = new System.Drawing.Size(146, 17);
+            this.lbl_time.TabIndex = 2;
+            this.lbl_time.Text = "编写时间：2013年4月1日";
+            // 
+            // lbl_tool
+            // 
+            this.lbl_tool.AutoSize = true;
+            this.lbl_tool.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbl_tool.Location = new System.Drawing.Point(22, 90);
+            this.lbl_tool.Name = "lbl_tool";
+            this.lbl_tool.Size = new System.Drawing.Size(156, 17);
+            this.lbl_tool.TabIndex = 3;
+            this.lbl_tool.Text = "编程工具：VS2015（C＃）";
+            // 
+            // btn_close
+            // 
+            this.btn_close.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_close.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_close.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_close.Location = new System.Drawing.Point(59, 114);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(75, 23);
+            this.btn_close.TabIndex = 4;
+            this.btn_close.Text = "关 闭";
+            this.btn_close.UseVisualStyleBackColor = false;
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            // 
+            // about
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(200, 150);
+            this.ControlBox = false;
+            this.Controls.Add(this.btn_close);
+            this.Controls.Add(this.lbl_name);
+            this.Controls.Add(this.lbl_tool);
+            this.Controls.Add(this.lbl_time);
+            this.Controls.Add(this.lbl_author);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "about";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "关于";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_author;
+        private System.Windows.Forms.Label lbl_name;
+        private System.Windows.Forms.Label lbl_time;
+        private System.Windows.Forms.Label lbl_tool;
+        private System.Windows.Forms.Button btn_close;
+    }
+}
